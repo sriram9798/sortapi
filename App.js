8bcebf1e1615f990c632a058a069a7da85e473a3
Prasanna Venkatesan <prasanna1792@gmail.com>
2020-09-26 15:37:09 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const port = 7000;
+const cors = require('cors');
+app.use(cors());
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+const MongoServer = require('mongodb');
+const MongoUrl = 'mongodb://localhost:27017';
+const MongoClient = MongoServer.MongoClient;
+let db;
+
+app.get('/restaurants/:mealtype', (req,res) => {
+    query = {"type.mealtype":req.params.mealtype};
+    sort_cost = {"cost": 1};
+
+    //Filter by price(high to low)
+    if(req.query.hTol) {
+        sort_cost = {"cost": Number(req.query.hTol)}
+    }
+
+    db.collection('restaurant').find(query).sort(sort_cost).toArray((err,result) => {
+        if(err) throw err;
+        res.send(result);
+    })
+})
+
+
+MongoClient.connect(MongoUrl,(err,client) => {
+    if(err) throw err; 
+    db = client.db('edurekainternship');
+    app.listen(port ,(err) => {
+        if(err) throw err;
+        console.log(`Server running on port ${port}`);
+    })
+})
